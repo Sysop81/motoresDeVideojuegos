295aec11eedeb30c5816e4206eb91e7ebd66c384
Jose Ramon Lopez Guillen <2280256@alu.murciaeduca.es>
2024-09-30 13:58:06 +0200

    EJ1 first commit

---
+++ Examples/Ejercicios_UT3/EJ1UT3/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    private GameObject plane;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        plane = GameObject.FindGameObjectWithTag("Player");
+        offset = new Vector3(20, 5, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
---
+++ Examples/Ejercicios_UT3/EJ1UT3/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed = 10f;
+    public float rotationSpeed = 45f;
+    public float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        //horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        // move the plane forward at a constant rate
+        transform.Translate(/*Vector3.back * speed*/ speed  * Time.deltaTime * Vector3.forward);
+
+        // tilt the plane up/down based on up/down arrow keys
+        this.transform.Rotate( rotationSpeed * verticalInput * Time.deltaTime * Vector3.right);
+        //transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+    }
+}
