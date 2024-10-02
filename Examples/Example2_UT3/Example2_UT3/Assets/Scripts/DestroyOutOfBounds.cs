4636760f4d6193dfb81aa54723a6d3da715ef167
Jose Ramon Lopez Guillen <2280256@alu.murciaeduca.es>
2024-10-02 13:56:06 +0200

    working in eaxmple 2 of UT3

---
+++ Examples/Example2_UT3/Example2_UT3/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+
+    [SerializeField] private float upBound = 30f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.z > upBound)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Examples/Example2_UT3/Example2_UT3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float horizontalInput;
+
+    [SerializeField] private float speed = 10f;
+
+    private const float _LIMIT = 15.7f;
+    
+    [SerializeField] private GameObject projectile;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get horizontal input
+        horizontalInput = Input.GetAxis("Horizontal");
+        
+        // Player move
+        this.transform.Translate( horizontalInput * speed * Time.deltaTime * Vector3.right);
+        
+        // Set left and rigth limit by tranform position 
+        if(transform.position.x < -_LIMIT)
+            this.transform.position = new Vector3(-_LIMIT, transform.position.y, transform.position.z);
+        
+        if(transform.position.x > _LIMIT)
+            this.transform.position = new Vector3(_LIMIT, transform.position.y, transform.position.z);
+        
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Fire1"))
+            Instantiate(projectile, this.transform.position, this.transform.rotation);
+    }
+}
