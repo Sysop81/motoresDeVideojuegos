7b5a360685ecda0248646145ac9749d3f14b8425
José Ramón López <jose.rlg27@gmail.com>
2024-10-04 15:14:57 +0200

    Adding InvokeRepeating funcion to SpawnManager

--- Examples/Example2_UT3/Example2_UT3/Assets/Scripts/SpawnManager.cs
+++ Examples/Example2_UT3/Example2_UT3/Assets/Scripts/SpawnManager.cs
@@ -15,13 +15,16 @@ public class spawnManager : MonoBehaviour
     void Start()
     {
         _spawnRangeZ = transform.position.z;
+        
+        // Automatic invoke
+        InvokeRepeating(nameof(CreateAnimal),0,2);
     }
 
     // Update is called once per frame
     void Update()
     {
         // If press kay S -> instanciate a new animal gameObject
-        if (Input.GetKeyDown(KeyCode.S))
+       /* if (Input.GetKeyDown(KeyCode.S))
         {
             // Generate a randon X axis position 
             var posX = Random.Range(-_spawnRangeX, _spawnRangeX);
@@ -34,6 +37,21 @@ public class spawnManager : MonoBehaviour
             
             // Instanciate a new animal
             Instantiate(animals[index], position, animals[index].transform.rotation);
-        }
+        } */
+    }
+
+    private void CreateAnimal()
+    {
+        // Generate a randon X axis position 
+        var posX = Random.Range(-_spawnRangeX, _spawnRangeX);
+            
+        // Generate a new vector 3 with the animal position
+        Vector3 position = new Vector3(posX, 0, _spawnRangeZ);
+            
+        // Get a randon index from animal, to get animal type.
+        index = Random.Range(0, animals.Length);
+            
+        // Instanciate a new animal
+        Instantiate(animals[index], position, animals[index].transform.rotation);
     }
 }
